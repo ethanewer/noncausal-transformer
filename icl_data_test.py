d884c37bd1bc11617b827bfea6d7baf672a3931c
Ethan <ewer@wisc.edu>
2024-07-17 13:53:26 -0500

    fixed icl_data_test

--- icl_data_test.py
+++ icl_data_test.py
@@ -7,65 +7,67 @@ import sys
 def test_lcg_zeros():
     lcg = LinearCurriculumGenerator(1, 3, 1, 1)
     x = torch.cat([lcg.generate() for _ in range(4)], dim=0)
-    
-    zeros = torch.tensor([
-        # first generate
-        [False, True, True],
-        [False, True, True],
-        [False, True, True],
-        [False, True, True],
-        # second generate
-        [False, False, True],
-        [False, True, True],
-        [False, False, True],
-        [False, True, True],
-        [False, False, True],
-        [False, True, True],
-        [False, False, True],
-        [False, True, True],
-        # third generate
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        # fourth generate
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-        [False, False, False],
-        [False, True, True],
-    ])
+
+    zeros = torch.tensor(
+        [
+            # first generate
+            [False, True, True],
+            [False, True, True],
+            [False, True, True],
+            [False, True, True],
+            # second generate
+            [False, False, True],
+            [False, True, True],
+            [False, False, True],
+            [False, True, True],
+            [False, False, True],
+            [False, True, True],
+            [False, False, True],
+            [False, True, True],
+            # third generate
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            # fourth generate
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+            [False, False, False],
+            [False, True, True],
+        ]
+    )
 
     assert torch.all(x[zeros] == 0)
 
 
 def test_lcg_linear_model(verbose=False):
-    lcg = LinearCurriculumGenerator(5, 20, 1, 1)
+    lcg = LinearCurriculumGenerator(100, 1000, 1, 100)
 
     for _ in range(25):
         z = lcg.generate()
-        x = z[::2]
+        x = z[::2, : lcg.start_dim]
         y = z[1::2, 0]
         w, *_ = torch.linalg.lstsq(x, y)
         y_hat = x @ w
         if verbose:
-           print(f"R^2: {(1 - F.mse_loss(y, y_hat) / torch.var(y)).item():.4f}")
-        assert torch.allclose(y, y_hat,rtol=1e-4, atol=1e-6)
+            print(f"R^2: {(1 - F.mse_loss(y, y_hat) / torch.var(y)).item():.4f}")
+        assert torch.allclose(y, y_hat, rtol=1e-4, atol=1e-4)
 
 
 if __name__ == "__main__":
