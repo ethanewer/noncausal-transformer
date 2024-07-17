16ff90b353c4cb73faf5d1e8770244f551f5e126
Ethan <ewer@wisc.edu>
2024-07-17 18:10:29 -0500

    train_icl_curriculum notebook

--- model/decoder_transformer_icl.py
+++ model/decoder_transformer_icl.py
@@ -4,7 +4,7 @@ from torch import nn, optim, Tensor
 from .base import *
 
 
-class DecoderTransformerStack_ICL(nn.Module):
+class DecoderTransformerStackICL(nn.Module):
     """Decoder transformer model without embeddings or lm-head, with optional causality."""
 
     def __init__(self, config: DecoderTransformerConfig) -> None:
@@ -14,13 +14,13 @@ class DecoderTransformerStack_ICL(nn.Module):
         self.loss_fn = self.config.loss_fn
         self.stack = nn.ModuleList([Block(config) for _ in range(config.n_layer)])
         self.stack[0].attn.is_causal = True
-        
+
         # new updates based on the paper
         self.wpe = nn.Embedding(config.block_size, config.n_embd)
         self.read_in = nn.Linear(config.n_dim, config.n_embd)
         self.read_out = nn.Linear(config.n_embd, 1)
         self.ln_f = nn.LayerNorm(config.n_embd, bias=config.bias)
-        
+
         self.apply(self.__init_weights)
         for p_name, p in self.named_parameters():
             if p_name.endswith("c_proj.weight"):
@@ -49,8 +49,8 @@ class DecoderTransformerStack_ICL(nn.Module):
             x = block(x)
 
         if forward_idxs is not None:
-            x = x[:, forward_idxs, :] 
-        
+            x = x[:, forward_idxs, :]
+
         x = self.ln_f(x)
         x = self.read_out(x)
 
@@ -58,7 +58,7 @@ class DecoderTransformerStack_ICL(nn.Module):
             if forward_idxs is not None:
                 y = y[:, forward_idxs, :]
 
-            loss = self.loss_fn(x[:,:,0], y[:,:,0])
+            loss = self.loss_fn(x[:, :, 0], y[:, :, 0])
 
             if backward:
                 loss.backward()
@@ -73,6 +73,7 @@ class DecoderTransformerStack_ICL(nn.Module):
         y: Tensor | None,
         backward: bool,
         forward_idxs: list[int] | None,
+        fast_backward: bool,
     ) -> tuple[Tensor, float | None]:
         """Helper method for forward. Noncausal forward is O(N^3.)"""
         if forward_idxs is None:
@@ -85,15 +86,14 @@ class DecoderTransformerStack_ICL(nn.Module):
             x_t = x[:, : t + 1, :]
             for block in self.stack[1:]:  # type: ignore
                 x_t = block(x_t)
-                
+
             x_t = self.ln_f(x_t)
             x_t = self.read_out(x_t)
 
             if y is not None and self.loss_fn is not None:
-                # loss = self.loss_fn(x_t[:, -1, :], y[:, t]) / len(forward_idxs)
                 loss = self.loss_fn(x_t[:, -1, 0], y[:, t, 0]) / len(forward_idxs)
-                loss_sum += loss.detach()
-                if backward:
+                loss_sum += loss if fast_backward else loss.detach()
+                if backward and not fast_backward:
                     loss.backward(retain_graph=True)
 
             logits.append(x_t[:, -1, :].detach())
@@ -101,6 +101,8 @@ class DecoderTransformerStack_ICL(nn.Module):
         logits = torch.stack(logits, dim=1)
         if y is not None and self.loss_fn is not None:
             loss = loss_sum
+            if backward and fast_backward:
+                loss.backward()
         else:
             loss = None
         return logits, loss
@@ -111,6 +113,7 @@ class DecoderTransformerStack_ICL(nn.Module):
         y=None,
         backward=False,
         forward_idxs: list[int] | None = None,
+        fast_backward=False,
     ) -> tuple[Tensor, float | None]:
         assert (
             x.shape[1] <= self.config.block_size
@@ -124,11 +127,11 @@ class DecoderTransformerStack_ICL(nn.Module):
         pos_emb = self.wpe(pos_idxs)
         x = self.read_in(x)
         x = x + pos_emb
-        
+
         if self.is_causal:
             return self.__causal_forward(x, y, backward, forward_idxs)
         else:
-            return self.__noncausal_forward(x, y, backward, forward_idxs)
+            return self.__noncausal_forward(x, y, backward, forward_idxs, fast_backward)
 
     def configure_optimizers(
         self,
